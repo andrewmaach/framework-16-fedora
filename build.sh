a7b27acf79938d0523e4e93cbd7519441e07337d
Andrew Maach <andy@andrewmaach.com>
2024-08-24 22:29:49 -0500

    cleanup zed installation

--- build.sh
+++ build.sh
@@ -11,13 +11,18 @@ wget https://proton.me/download/mail/linux/ProtonMail-desktop-beta.rpm
 rpm-ostree install ProtonMail-desktop-beta.rpm
 rm ProtonMail-desktop-beta.rpm
 
+wget https://zed.dev/api/releases/stable/latest/zed-linux-x86_64.tar.gz
+tar -xvf zed-linux-x86_64.tar.gz -C /opt
+ln -sf ~/opt/zed.app/bin/zed /bin/zed
+rm zed-linux-x86_64.tar.gz
+
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install fira-code-fonts python3-pip containerd.io podman-compose docker-compose-plugin zed
+rpm-ostree install fira-code-fonts python3-pip
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
