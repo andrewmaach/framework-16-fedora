35fdd5a870b129c4091490c66b2e0e1987714094
Andrew Maach <andy@andrewmaach.com>
2024-08-25 15:02:21 -0500

    cleanup build script

--- build.sh
+++ build.sh
@@ -4,26 +4,33 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+# Additional repos
+echo "Installing Terra"
+curl -fsSL https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo | pkexec tee /etc/yum.repos.d/terra.repo
+rpm-ostree install terra-release
 
 ### Install packages
 
+echo "Downloading packages"
 
 # ProtonMail Desktop
 wget https://proton.me/download/mail/linux/ProtonMail-desktop-beta.rpm
-rpm-ostree install ProtonMail-desktop-beta.rpm
-rm ProtonMail-desktop-beta.rpm
-
-# Warp Terminal
 wget -O warp.rpm https://app.warp.dev/download?package=rpm
-rpm-ostree install warp.rpm
 
-curl -fsSL https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo | pkexec tee /etc/yum.repos.d/terra.repo
-sudo rpm-ostree install terra-release
+
+
+echo "Installing local packages"
+# Warp Terminal
+rpm-ostree install warp.rpm ProtonMail-desktop-beta.rpm
+
+rm ProtonMail-desktop-beta.rpm warp.rpm
 
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+
+echo "Installing Fedora packages..."
 
 # this installs a package from fedora repos
 rpm-ostree install fira-code-fonts python3-pip zed
