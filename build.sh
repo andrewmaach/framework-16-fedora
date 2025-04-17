fea4f6955ae01cc67304892a7093b4aa6796ad18
Andrew Maach <dev@andrewmaach.com>
2025-04-17 13:34:32 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,13 +9,6 @@ RELEASE="$(rpm -E %fedora)"
 
 echo "Downloading packages"
 
-# ProtonMail Desktop
-wget https://proton.me/download/mail/linux/ProtonMail-desktop-beta.rpm
-
-
-echo "Installing local packages"
-rpm-ostree install ProtonMail-desktop-beta.rpm
-
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
@@ -31,6 +24,19 @@ rpm-ostree install \
     nautilus \
     containerd.io \
     fish \
+    virt-install \
+    libvirt-daemon-config-network \
+    libvirt-daemon-kvm \
+    qemu-kvm \
+    virt-manager \
+    virt-viewer \
+    virt-top \
+    gcc \
+    gnome-extensions-app \
+    postgresql \
+    simple-scan \
+    zbar \
+    tesseract \
     librewolf
 
 # this would install a package from rpmfusion
@@ -39,3 +45,4 @@ rpm-ostree install \
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl enable libvirtd
